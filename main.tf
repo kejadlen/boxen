46af2ca9823e40daae2c432b96189429e10fcf71
Alpha Chen <alpha@kejadlen.dev>
2022-05-11 16:41:42 -0700

    first attempt

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  cloud {
+    organization = "kejadlen"
+    workspaces {
+      name = "boxen"
+    }
+  }
+}
+
+variable "GOOGLE_CREDENTIALS" {}
+
+provider "google" {
+  project = "arbitrary-definitions-349923"
+  region  = "us-west1"
+}
+
+module "google_image_nixos" {
+  source        = "github.com/tweag/terraform-nixos/google_image_nixos"
+  nixos_version = "latest"
+}
+
+resource "google_compute_instance" "imperishable_night" {
+  name         = "Imperishable Night"
+  machine_type = "e2-standard-2"
+  zone         = "us-west1-a"
+
+  boot_disk {
+    initialize_params {
+      image = module.google_image_nixos.self_link
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
